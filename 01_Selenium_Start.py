f2f8215a5733c6a8512c28ecbe4328b6757280d2
009MHz <mharisbsatriyo@gmail.com>
2023-01-01 15:44:25 +0700

    selenium validate the elements content

--- 01_Selenium_Start.py
+++ 01_Selenium_Start.py
@@ -2,11 +2,31 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-time.sleep(5)
 
-#Open the page
+# Open the page
 driver.get("https://practicetestautomation.com/practice-test-login/")
 time.sleep(5)
+
+# Type username student into Username field
+username_locator = driver.find_element(By.ID, "username")  # finding the elements by ID
+
+# Type password Password123 into Password field
+password_locator = driver.find_element(By.NAME, "password")  # finding the elements by the name
+
+# Hit Submit button
+submit_locator = driver.find_element(By.XPATH, "//button[@class='btn']")  # finding the elements by Xpath
+
+"""Verify new page URL contains proper elements"""
+# success_header_loc = driver.find_element(By.XPATH, "//h1[@class='post-title']") # finding the elements by Xpath
+success_header_loc = driver.find_element(By.TAG_NAME, "h1")  #finding elements by TAG_NAME
+
+# Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+success_subhead_loc = driver.find_element(By.XPATH, "//strong")
+
+# Verify button Log out is displayed on the new page
+logout_link = driver.find_element(By.XPATH, "//a[@href='https://practicetestautomation.com/practice-test-login/']")
+
