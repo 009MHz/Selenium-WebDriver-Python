e964c6a82c1fd0e9678a3c382b89a36da57b825d
009MHz <mharisbsatriyo@gmail.com>
2023-01-01 12:59:50 +0700

    Initial Push

---
+++ 01_Selenium_Start.py
@@ -0,0 +1,12 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+time.sleep(5)
+
+"""Open the page"""
+driver.get("https://practicetestautomation.com/practice-test-login/")
+time.sleep(5)
