c084185aa69bb8757c69d55a013a56f03ac21cd3
009MHz <mharisbsatriyo@gmail.com>
2023-01-05 01:11:45 +0700

    removing duplicated comment

--- 01_Selenium_Start.py
+++ 01_Selenium_Start.py
@@ -16,11 +16,11 @@ username_locator = driver.find_element(By.ID, "username")  # finding the element
 username_locator.send_keys("student")  # inserting given value into the fields
 
 # Type password Password123 into Password field
-password_locator = driver.find_element(By.NAME, "password")  # finding the elements by the name
-password_locator.send_keys("Password123")  # inserting given value into the fields
+password_locator = driver.find_element(By.NAME, "password")
+password_locator.send_keys("Password123")
 
 # Hit Submit button
-submit_locator = driver.find_element(By.XPATH, "//button[@class='btn']")  # finding the elements by Xpath
+submit_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
 submit_locator.click()  # calling the click function from submit elements instance
 time.sleep(3)  # adding a waiting time for page is loaded before below scripts is executed
 
@@ -30,14 +30,14 @@ actual_URL = driver.current_url  # property to validating the page
 assert actual_URL == "https://practicetestautomation.com/logged-in-successfully/"  # validating the page
 
 # The Page is displaying Logged In Successfully header
-success_header_loc = driver.find_element(By.TAG_NAME, "h1")  # finding elements by TAG_NAME (class name)
+success_header_loc = driver.find_element(By.TAG_NAME, "h1")
 actual_header = success_header_loc.text  # property to validating the elements
 assert actual_header == "Logged In Successfully"  # Validating the header text
 
 # New page contains expected text ('Congratulations' or 'successfully logged in')
-success_subhead_loc = driver.find_element(By.TAG_NAME, "p")  # finding elements by TAG_NAME
-actual_subhead = success_subhead_loc.text  # property to validating the elements
-assert actual_subhead == "Congratulations student. You successfully logged in!"  # Validating the message text
+success_subhead_loc = driver.find_element(By.TAG_NAME, "p")
+actual_subhead = success_subhead_loc.text
+assert actual_subhead == "Congratulations student. You successfully logged in!"
 
 # Log out CTA is displayed on the new page
 logout_link = driver.find_element(By.LINK_TEXT, 'Log out')
