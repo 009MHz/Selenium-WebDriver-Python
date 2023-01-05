fe08f988fbffb8480221f3313093958173949730
009MHz <mharisbsatriyo@gmail.com>
2023-01-05 22:28:20 +0700

    pycharm pro reading

--- test/test_login_page.py
+++ test/test_login_page.py
@@ -5,40 +5,43 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-# Open the page
-driver.get("https://practicetestautomation.com/practice-test-login/")
-time.sleep(2)
+class TestPositiveLogin:
+    def test_positive_login(self):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-# Type username student into Username field
-username_locator = driver.find_element(By.ID, "username")
-username_locator.send_keys("student")
+        # Open the page
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+        time.sleep(2)
 
-# Type password Password123 into Password field
-password_locator = driver.find_element(By.NAME, "password")
-password_locator.send_keys("Password123")
+        # Type username student into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("student")
 
-# Hit Submit button
-submit_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
-submit_locator.click()
-time.sleep(3)
+        # Type password Password123 into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("Password123")
 
-"""Verify new page URL contains proper elements"""
-# User is directed to correct page  (https://practicetestautomation.com/logged-in-successfully/)
-actual_URL = driver.current_url
-assert actual_URL == "https://practicetestautomation.com/logged-in-successfully/"
+        # Hit Submit button
+        submit_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        submit_locator.click()
+        time.sleep(3)
 
-# The Page is displaying Logged In Successfully header
-success_header_loc = driver.find_element(By.TAG_NAME, "h1")
-actual_header = success_header_loc.text
-assert actual_header == "Logged In Successfully"
+        """Verify new page URL contains proper elements"""
+        # User is directed to correct page
+        actual_URL = driver.current_url
+        assert actual_URL == "https://practicetestautomation.com/logged-in-successfully/"
 
-# New page contains expected text ('Congratulations' or 'successfully logged in')
-success_subhead_loc = driver.find_element(By.TAG_NAME, "p")
-actual_subhead = success_subhead_loc.text
-assert actual_subhead == "Congratulations student. You successfully logged in!"
+        # The Page is displaying Logged In Successfully header
+        success_header_loc = driver.find_element(By.TAG_NAME, "h1")
+        actual_header = success_header_loc.text
+        assert actual_header == "Logged In Successfully"
 
-# Log out CTA is displayed on the new page
-logout_link = driver.find_element(By.LINK_TEXT, 'Log out')
-assert logout_link.is_displayed()
+        # New page contains expected text ('Congratulations' or 'successfully logged in')
+        success_subhead_loc = driver.find_element(By.TAG_NAME, "p")
+        actual_subhead = success_subhead_loc.text
+        assert actual_subhead == "Congratulations student. You successfully logged in!"
+
+        # Log out CTA is displayed on the new page
+        logout_link = driver.find_element(By.LINK_TEXT, 'Log out')
+        assert logout_link.is_displayed()
