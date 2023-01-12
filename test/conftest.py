0adca5f7eaa58d5c7a141ec09a9211a90cb496ec
009MHz <mharisbsatriyo@gmail.com>
2023-01-12 11:40:01 +0700

    adding @pytest.fixture(params) comment out pytest_addoption(parser)

--- test/conftest.py
+++ test/conftest.py
@@ -1,28 +1,30 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
-@pytest.fixture()
+
+@pytest.fixture(params=["chrome", "edge"])
 def driver(request):
-    browser = request.config.getoption("--browsers")
-    print(f"Creating {browser} Driver...")
+    # browser = request.config.getoption("--browsers")
+    browser = request.param
+    print(f"Creating virtual {browser} Driver...")
     if browser == "chrome":
-        test_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        test_driver = webdriver.Chrome(ChromeDriverManager().install())
     elif browser == "edge":
         test_driver = webdriver.Edge(EdgeChromiumDriverManager().install())
     else:
         raise TypeError(f"'{browser}' isn't installed/provided")
     yield test_driver
-    print(f"\n{browser} Driver is created, exiting the processes...\n")
+    print(f"\n{browser} Driver is created, exiting the processes.\n")
     test_driver.quit()
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--browsers",
-        action="store",
-        default="chrome",
-        help="browsers to run the tests (Chrome/Edge)"
-    )
+
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         "--browsers",
+#         action="store",
+#         default="chrome",
+#         help="browsers to run the tests (Chrome/Edge)"
+#     )
