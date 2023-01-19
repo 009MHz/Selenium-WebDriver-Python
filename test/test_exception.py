6654fa39fc1d8d2a8f4c3fa264e5da66de329a4a
009MHz <mharisbsatriyo@gmail.com>
2023-01-19 12:05:51 +0700

    adding test case 2 exception

--- test/test_exception.py
+++ test/test_exception.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import visibility_of_element_located as elem_vis, \
-    presence_of_element_located as elem_loc
+    presence_of_element_located as elem_loc, invisibility_of_element as dismiss
 from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestException:
     @pytest.mark.exception
-    def test_element_exception(self, driver):
+    def test_no_element_exception(self, driver):
         # Open the page
         driver.get("https://practicetestautomation.com/practice-test-exceptions/")
 
@@ -24,15 +24,36 @@ class TestException:
         """Row 2 Is displayed"""
         wait = WebDriverWait(driver, 15)
 
-        # Verify the Toastbar is displayed
-        toast_locator = wait.until(elem_vis((By.XPATH, "//div[@id='confirmation']")))
+        # Verify the Toast bar is displayed
+        toast_locator = wait.until(elem_vis((By.ID, "confirmation")))
         actual_toast_msg = toast_locator.text
-        assert actual_toast_msg == "Row 2 was added"
+        assert actual_toast_msg == "Row 2 was added", "Added Toast Message is incorrect"
 
         # Verify Row 2 availability
         row2_locator = wait.until(elem_loc((By.ID, "row2")))
         assert row2_locator.is_displayed(), "Row 2 Is missing"
 
+    @pytest.mark.debug
+    @pytest.mark.exception
+    def test_not_intractable_element(self, driver):
+        # Open The Page
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
 
+        "Click Add button on Row 1"
+        driver.find_element(By.ID, "add_btn").click()
 
+        "Type text into the second input field"
+        # Pass the text to the text box
+        row2_input_locator = driver.find_element(By.XPATH, "//div[@id='row2']/input")
+        row2_input_locator.send_keys("Written using python selenium")
 
+        "Push Save button using locator By.name('Save')"
+        # save_btn_locator = driver.find_element(By.NAME, "Save") # buggy script
+        save_btn_locator = driver.find_element(By.XPATH, "//div[@id='row2']/button[@name='Save']")
+        save_btn_locator.click()
+
+        "Verify text saved"
+        wait = WebDriverWait(driver, 30)
+        save_toast_locator = wait.until(elem_vis((By.ID, "confirmation")))
+        actual_save_toast = save_toast_locator.text
+        assert actual_save_toast == "Row 2 was saved", "Saved Toast Message is incorrect"
