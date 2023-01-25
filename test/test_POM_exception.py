8115e58424a8b546c3d905611b04749f198af092
009MHz <mharisbsatriyo@gmail.com>
2023-01-25 17:10:47 +0700

    converting stale_element comments into test script

--- test/test_POM_exception.py
+++ test/test_POM_exception.py
@@ -1,13 +1,13 @@
 import time
 
 import pytest
-from page_objects.exception_page import ExcPage
+from page_objects.exception_page import ExceptionPage
 
 
 class TestException:
     @pytest.mark.exception
     def test_no_element_exception(self, driver):
-        exc = ExcPage(driver)
+        exc = ExceptionPage(driver)
         exc.open_page()
         assert exc.r1_presence(), "Row 1 Can't be found on the page"
         exc.r1_hit_add()
@@ -17,7 +17,7 @@ class TestException:
 
     @pytest.mark.exception
     def test_not_interactable_element(self, driver):
-        exc = ExcPage(driver)
+        exc = ExceptionPage(driver)
         exc.open_page()
         exc.r1_hit_add()
         exc.r2_type_text("Written by Python Selenium")
@@ -27,21 +27,21 @@ class TestException:
 
     @pytest.mark.exception
     def test_invalid_state(self, driver):
-        exception = ExcPage(driver)
-        exception.open_page()
-        exception.r1_hit_edit()
-        assert not exception.r1_input_presence(), "Row 1 textbox is disabled "
-        exception.r1_input_clear()
-        exception.r1_type_text("Written using Selenium")
-        exception.r1_hit_save()
-        assert exception.toast_presence(), "Saved toast bar is missing"
-        assert exception.toast_text() == "Row 1 was saved", "Unexpected toast bar message is found"
+        exc = ExceptionPage(driver)
+        exc.open_page()
+        exc.r1_hit_edit()
+        assert not exc.r1_input_presence(), "Row 1 textbox is disabled "
+        exc.r1_input_clear()
+        exc.r1_type_text("Written using Selenium")
+        exc.r1_hit_save()
+        assert exc.toast_presence(), "Saved toast bar is missing"
+        assert exc.toast_text() == "Row 1 was saved", "Unexpected toast bar message is found"
 
     @pytest.mark.exception
     @pytest.mark.debug
     def test_stale_element_reference(self, driver):
-        pass
-        # Open page
-        "Find the instructions text element"
-        "Hit Add Button"
-        "Instruction text should be disappear"
+        exc = ExceptionPage(driver)
+        exc.open_page()
+        exc.inst_presence()
+        exc.r1_hit_add()
+        assert not exc.inst_absence(), "Instruction text still exist"
