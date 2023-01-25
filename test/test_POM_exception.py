449055fd0c8fbae94aff219192749e12fb0a71fc
009MHz <mharisbsatriyo@gmail.com>
2023-01-25 14:00:46 +0700

    clearing the supporting comments

--- test/test_POM_exception.py
+++ test/test_POM_exception.py
@@ -4,10 +4,6 @@ from page_objects.exception_page import ExcPage
 
 class TestException:
     @pytest.mark.exception
-<<<<<<< HEAD
-=======
-    @pytest.mark.debug
->>>>>>> origin/master
     def test_no_element_exception(self, driver):
         exc = ExcPage(driver)
         exc.open_page()
@@ -16,25 +12,14 @@ class TestException:
         exc.toast_presence()
         assert exc.r2_presence(), "Row 2 can't be located on the page"
         assert exc.toast_text() == "Row 2 was added", "Added Toast Message is incorrect"
-<<<<<<< HEAD
 
     @pytest.mark.exception
     @pytest.mark.debug
     def test_not_intractable_element(self, driver):
         exc = ExcPage(driver)
-        # Open The Page
         exc.open_page()
-        "Click Add button on Row 1"
         exc.r1_hit_add()
-        "Type text into the second input field"
-        # Pass the text to the text box
         exc.r2_type_text("Written by Python Selenium")
-        "Push Save button using locator By.name('Save')"
         exc.r2_hit_save()
-        "Verify text saved"
-        # Verify the toast appearance
         assert exc.toast_presence(), "Saved toast bar can't be found on the page"
-        # Verify the toast message
         assert exc.toast_text() == "Row 2 was saved", "Saved Toast Message is incorrect"
-=======
->>>>>>> origin/master
