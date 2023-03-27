8945f3a274923b4f891d98b1e663ad8fe8478231
MHB8700 <mharisbsatriyo@gmail.com>
2023-03-27 22:46:16 +0700

    aku nganti lali changes e opo wae

--- test/privy_multiple_signers.py
+++ test/privy_multiple_signers.py
@@ -7,8 +7,8 @@ from selenium.webdriver.common.by import By
 
 
 class TestMultipleAddress:
-    def login(self, driver):
-        wait = WebDriverWait(driver, 15)
+    def test_login(self, driver):
+        wait = WebDriverWait(driver, 20)
         """Login The Page"""
         # Open The page
         driver.get("https://app.privy.id/dashboard")
@@ -25,31 +25,38 @@ class TestMultipleAddress:
         cont_button = wait.until(ec.element_to_be_clickable((By.ID, "tag-lg001")))
         cont_button.click()
 
-        # Switch to Enterprise
-        ava_loc = wait.until((ec.presence_of_element_located((By.ID, "v-step-3__BV_button_"))))
-        ava_loc.click()
+        "Switch to Enterprise"
+        # Click Avatar
+        wait.until((ec.presence_of_element_located((By.CSS_SELECTOR, ".avatar:nth-child(2) > .avatar__img")))).click()
 
-        enterp_loc = wait.until(ec.visibility_of_element_located((By.ID, "__BVID__51___BV_tab_button__")))
-        enterp_loc.click()
+        # Click Enterprise
+        wait.until(ec.visibility_of_element_located((By.ID, "__BVID__51___BV_tab_button__"))).click()
 
-        demo_loc = wait.until(ec.presence_of_element_located((By.XPATH, "//li[@class='d-flex align-items-center'][2]")))
-        demo_loc.click()
+        # Select Privy Demo
+        wait.until(ec.visibility_of_element_located((By.XPATH, "//li[@class='d-flex align-items-center'][2]"))).click()
 
-        # Upload Documents
-        # upload_sidebar
+        "Upload Documents"
+        # Click Upload Button
+        time.sleep(5)
+        wait.until(ec.visibility_of_element_located((By.ID, 'v-step-0'))).click()
+
+        # Hit direct URL
         driver.get("https://app.privy.id/upload/share-only/upload")
-        # wait.until(ec.visibility_of_element_located((By.ID, 'v-step-0'))).click()
 
-        # hit request from other
-        # wait.until(ec.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Request From Others"))).click()
+        # Select request from other
+        wait.until(ec.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Request From Others"))).click()
 
         # hit browse
-        wait.until(ec.visibility_of_element_located((By.PARTIAL_LINK_TEXT, "browse"))).click()
+        #wait.until(ec.visibility_of_element_located((By.PARTIAL_LINK_TEXT, "browse"))).click()
 
         # Upload the docs
-        upload_docs = driver.find_element(By.XPATH, "//input[@id='fileSelector']")
-        # upload_docs = driver.find_element(By.XPATH, "//input[@name='filename']")
-        upload_docs.send_keys("/Users/MHB8700/dummy_docs_2.pdf")
+
+        # Find the hidden input element
+        file_input = driver.find_element(By.ID, "fileSelector")
+
+        # Execute JavaScript to set the value of the input element
+        driver.execute_script("arguments[0].style.display = 'none';", file_input)
+        file_input.send_keys("/Users/MHB8700/dummy_docs_2.pdf")
 
         # upload validation
         upload_file = wait.until(ec.presence_of_element_located((By.XPATH, "//button[@class='btn btn-danger']")))
@@ -60,39 +67,44 @@ class TestMultipleAddress:
         time.sleep(10)
 
     @pytest.mark.debug
-    def test_add_recipients(self, driver):
-        wait = WebDriverWait(driver, 10)
+    def add_recipients(self, driver):
+        wait = WebDriverWait(driver, 20)
         driver.get("https://app.privy.id/upload/share-only/share?docToken=cdc3c8b07fecf76bb87dbc32cac1d6b52f4e8567dae4a6fd9a471d25ff0ae004")
 
         "login with the correct user"
-        # Pass the user & Password
+        # Type the username
         username_locator = wait.until(ec.element_to_be_clickable((By.ID, "__BVID__4")))
         username_locator.send_keys("MHB8700")
         cont_button = wait.until(ec.element_to_be_clickable((By.ID, "tag-lg001")))
         cont_button.click()
         cont_button.click()
-        # Pass the password
+
+        # Type the password
         password_locator = wait.until(ec.element_to_be_clickable((By.ID, "__BVID__6")))
         password_locator.send_keys("Geo11787")
         cont_button = wait.until(ec.element_to_be_clickable((By.ID, "tag-lg001")))
         cont_button.click()
 
-        "Setup the method"
-        # wait for the overlay element to disappear
-        wait.until(ec.invisibility_of_element_located((By.CLASS_NAME, "overlay overlay-dark")))
+        # Wait for the overlay to become invisible
+        time.sleep(10)
+        # overlay_locator = (By.CLASS_NAME, "overlay overlay-dark")
+        # wait.until(ec.invisibility_of_element_located(overlay_locator))
 
-        # scroll the radio button element into view
-        wait.until(ec.visibility_of_element_located(
-            (By.XPATH, "//div[@class='custom-control custom-control-inline custom-radio'][2]")))
-        wait.until(ec.element_to_be_clickable((By.XPATH, "//div[@class='custom-control custom-control-inline custom-radio'][2]"))).click()
+        # Wait for the radio button to become clickable and click it
+        radio_locator = (By.XPATH, "//div[@class='custom-control custom-control-inline custom-radio'][2]")
+        radio = wait.until(ec.element_to_be_clickable(radio_locator))
+        radio.click()
 
-
-        wait.until(ec.visibility_of_element_located((By.ID, "v-recipient-1")))  # add recipient button
         wait.until(ec.element_to_be_clickable((By.ID, "v-recipient-1"))).click()
 
         # Type the recipient
-        textbox = wait.until(ec.element_to_be_clickable(By. XPATH, "//input[@class='multiselect__input']"))
+        textbox = wait.until(ec.element_to_be_clickable((By. CLASS_NAME, "multiselect__tags")))
+        textbox.click()
         textbox.send_keys("mse.01@mailinator.com")
+        # new_path = (By.CSS_SELECTOR, "input.multiselect__input[style='width: 100%;']")
+        # textbox = wait.until(ec.visibility_of_element_located(new_path))
+        # textbox.click()
+
         time.sleep(10)
 
 
